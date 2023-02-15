0bcf40ce5763f7329bb1931e4ad575b4e5441cbd
jimmyGuerra <jimmy.j.guerra22@gmail.com>
2023-02-15 16:07:38 -0400

    anexando funcionalidad al boton de menu en mobile

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
+//Desktop menu
 const desktopmenu = document.querySelector('.desktop-menu');
-const navEmail = document.querySelector('.navbar-email')
+const navEmail = document.querySelector('.navbar-email');
 
 navEmail.addEventListener('click', toggleDesktopMenu);
+
+//Mobile Menu
+const iconMenuMobile = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+iconMenuMobile.addEventListener('click', toggleMobileMenu)
+
+
+
 
 function toggleDesktopMenu(){
     //de esta manera se resume todo el condicional que se encuentra comentado
@@ -14,4 +24,8 @@ function toggleDesktopMenu(){
         desktopmenu.classList.add('inactive');
         console.log(desktopmenu.getAttribute('class'));
     }*/
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
